546238cdad7ddd2b0c0cca7d35278434dd76de0c
Concourse <concourse@finbourne.com>
2023-06-15 15:28:11 +0000

    Update SDK 0.1.744 [publish]

--- sdk/lusid_notification/api/__init__.py
+++ sdk/lusid_notification/api/__init__.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import
 
 # import apis into api package
 from lusid_notification.api.application_metadata_api import ApplicationMetadataApi
-from lusid_notification.api.deliveries_api import DeliveriesApi
 from lusid_notification.api.event_types_api import EventTypesApi
 from lusid_notification.api.manual_event_api import ManualEventApi
 from lusid_notification.api.notifications_api import NotificationsApi
